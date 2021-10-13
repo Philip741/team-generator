c06c7b5bfff9449a3318c3db61b031d9da0a8c77
Philip Browning <philip741@gmail.com>
2021-10-13 14:43:41 -0400

    Created tests for all class files using jest

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,16 @@
+const { TestWatcher } = require('@jest/core');
+const Employee = require('../lib/Employee');
+
+let employee = new Employee("testEmployee","12345", "testEmail@email.com", "testRole");
+test("employee.getRole() should return role argument passed to Employee class", () => {
+    expect(employee.getRole()).toBe("testRole");
+});
+test("employee.getName() should return name argument passed to Employee class", () => {
+    expect(employee.getName()).toBe("testEmployee");
+});
+test("employee.getId() should return the ID passed to Employee class", () => {
+    expect(employee.getId()).toBe("12345");
+});
+test("employee.getEmail() should return the email passed to Employee class", () => {
+    expect(employee.getEmail()).toBe("testEmail@email.com");
+});
